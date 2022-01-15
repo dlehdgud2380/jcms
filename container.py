e44c949752419e179457b4b09fcf337332234cf0
dlehdgud2380 <dlehdgud2380@gmail.com>
2022-01-16 00:26:54 +0900

    add: logger class

--- container.py
+++ container.py
@@ -1,14 +1,17 @@
 # 쥬피터 컨테이너 관리하는 모듈 by Watson
 
 # 라이브러리 선언을 위한 import
+from dis import dis
 from os import system, listdir
 import time
 import socket
 import subprocess
 import re
+import threading
+import datetime
 
 # 타입힌트를 위한 import
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List
 
 # 경로 관련된 변수들 선언
 home_path: str = None # user 홈 폴더
@@ -43,7 +46,6 @@ work_path: str = f"{home_path}/jupyter_management_storage"
 
 
 # 컨테이너 관리하는 클래스
-# 객체 하나당 한개의 컨테이너를 관리함
 class Container:
     
     # 자동으로 컨테이너 생성하고 정보 저장
@@ -57,11 +59,10 @@ class Container:
         }
         self.init() # 설정한 컨테이너 이름으로 컨테이너 만들고 자동 실행
 
-    # 커스텀 도커 이미지 있는 지 체크 하는 함수
-    def check_image(self) -> None:
+    # 커스텀 도커 이미지 있는 지 체크 하는 함수(Private)
+    def __check_image(self) -> None:
         # 이미지 존재하는 지 확인
         image_check: str = command("docker images")
-
         # 존재하면 pass, 존재하지 않으면 docker image build
         if "image_watson_jupyter" not in image_check.split() :
             print("Not Ready: 커스텀 도커 이미지가 존재하지 않음 -> 이미지 빌드 실행")
@@ -71,16 +72,10 @@ class Container:
 
     # 컨테이너 생성할때 사용하는 함수
     def init(self) -> None:
-        # 이미지 있는 지 체크
-        self.check_image()
-
-        # 생성할 컨테이너 네임 입력하기
-        # container_name: str = input("새로 만들 주피터 컨테이너명 입력: jupyter_")
-
+        self.__check_image() # 이미지 있는 지 체크
         # 포트설정(기본 8888)
         host_port: str = self.port["hostPort"] # host
         cont_port: str = self.port["conPort"] # container
-
         # 사용중인 컨테이너 포트 확인 하기
         port_check: str = command("docker ps -a").split()
         while True:
@@ -90,13 +85,16 @@ class Container:
             # 존재하면 호스트 포트 1씩 증가시키기
             else:
                 host_port += 1
-
         # 하나의 컨테이너의 정보를 담는 디렉터리 생성하기
         system(f"mkdir {work_path}/{self.container_name}")
-            
         # 컨테이너 생성 하기
         # print(f"docker run -d -it -p {host_port}:{cont_port} --name jupyter_{container_name} --hostname jupyter_{container_name} -v {work_path}/{container_name}:/container_info image_watson_jupyter:0.0.1")
-        docker_run_command: str = f"docker run -d -it -p {host_port}:{cont_port} --name {self.container_name} --hostname {self.container_name} -v {work_path}/{self.container_name}:/container_info image_watson_jupyter:0.0.1"
+        docker_run_command: str = f"docker run -d -it " \
+                                f"-p {host_port}:{cont_port} " \
+                                f"--name {self.container_name} " \
+                                f"--hostname {self.container_name} " \
+                                f"-v {work_path}/{self.container_name}:/container_info image_watson_jupyter:0.0.1"
+        
         self.container_id = command(docker_run_command).strip()
         # print(f"id: {self.container_id}")
 
@@ -105,9 +103,9 @@ class Container:
         # 컨테이너 시작
         system(f"docker start {self.container_name} ")
 
-    # 컨테이너 종료 하는 함수
+    # 컨테이너 정지 하는 함수
     def stop(self) -> None:
-        # 컨테이너 제거하기
+        # 컨테이너 정지
         system(f"docker stop {self.container_name} ")
 
     # 컨테이너 삭제 하는 함수
@@ -126,18 +124,16 @@ class Container:
     # Option = True -> tuple형태로 리턴도 하고 print도 함
     # Option = Flase -> print로 정보만 보여줌 (기본값임)
     def info(self, option: bool = False) -> Dict:
-
         host_port: str = self.port["hostPort"] # host포트 가져오기
-
         # 컨테이너 정보가 담긴 Dict
         container_info: Dict = {
             "conId" : self.container_id,
             "conName" : self.container_name,
             "port" : host_port
         }
-
         print(f"[Container Info]\nid: {self.container_id}\nname: {self.container_name}\nport: {host_port}")
         if option == True : return container_info
+        
 
 # 컨테이너 내의 쥬피터 관련 된 정보 가져오는 클래스
 class JupyterInfo:
@@ -160,10 +156,6 @@ class JupyterInfo:
             "token" : None
         }
 
-    # log를 뽑아내는 함수
-    def log(self) -> None:
-        pass
-
     # Jupyter 연결에 필요한 정보를 가져와 저장하는 함수
     def info(self) -> None:
 
@@ -172,7 +164,7 @@ class JupyterInfo:
         regex_token: Any = re.compile("=[0-9a-zA-Z]+") # 토큰 정보 뽑는 정규식
 
         # port와 token 정보 가져오기
-        time.sleep(3) # 쥬피터 서버 구동을 기다리기 위해 3초 딜레이 
+        time.sleep(2) # 쥬피터 서버 구동을 기다리기 위해 2초 딜레이 
         output: str = command(f"docker exec -it {self.container_name} jupyter notebook list")
 
         # port와 token 정보 connection_info 에 넣기
@@ -189,16 +181,42 @@ class JupyterInfo:
         f.write(f"Port: {self.connection_info['port']}\n")
         f.write(f"Token: {self.connection_info['token']}\n")
         f.close()
-    
-    # 연결에 필요한 정보를 컨테이너쪽으로 요청하여 이메일로 보낼 수 있도록 하는 함수
-    def send_email() -> None:
-        pass
-        
+
+
+# 로그 기록 하는 쓰레드 클래스
+class Logger(threading.Thread):
+    def __init__(self, container_name: str, display: bool = False) -> None:
+        threading.Thread.__init__(self)
+        self.container_name = "jupyter_"+ container_name
+        self.display = display # 터미널상에 log를 띄울것인지 아닌지
+        print(f"[logger thread start - {display}]")
+
+    # 컨테이너 로그 기록 실행하는 함수
+    # 컨테이너 종료되면 같이 종료됨
+    def run(self) -> None:
+        option: str = None # 기록 옵션(터미널에 띄울것인지 아닌지)
+        if self.display == True:
+            option = "| tee"
+        else:
+            option = ">"
+        system(f"docker logs -f {self.container_name} {option} {work_path}/{self.container_name}/log.txt")
+        print("[logger thread end]")
+
 
 # 테스트용
 if __name__ == "__main__" :
+    print("------------container make && print info------------")
     jupyter_test1 = Container("test1")
     con_info = jupyter_test1.info(True)
+    print("\n\n------------jupyter info------------")
     jupyter_info = JupyterInfo(con_info)
     jupyter_info.info()
+    print("\n\n------------print connection_info.txt------------")
+    system(f"cat {work_path}/{con_info['conName']}/connection_info.txt")
+    print("\n\n------------logging------------")
+    logger = Logger("test1")
+    logger.daemon = True
+    logger.start()
+    #print("\n\n------------delete container------------")
     #jupyter_test1.remove()
+  
