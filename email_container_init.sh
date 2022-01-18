0d6c8609751ebf012eff3a9112176fdf95695cc7
dlehdgud2380 <dlehdgud2380@gmail.com>
2022-01-18 22:25:12 +0900

    fix: permission edit

--- email_container_init.sh
+++ email_container_init.sh
@@ -2,4 +2,8 @@
 # MailServer container make script
 
 docker run -it -d -p 587:587 -v $HOME/jupyter_management_storage:/jupyter_management_storage --name juMailServer --hostname juMailServer ubuntu:18.04
+docker exec juMailServer apt update 
+docker exec juMailServer apt install -y wget
+docker exec juMailServer wget https://raw.githubusercontent.com/dlehdgud2380/jupyter_container_management_system/beta/mailsetting.sh
+docker exec juMailServer chmod 777 mailsetting.sh
 docker exec -it juMailServer /bin/bash
--- mailsetting.sh
+++ mailsetting.sh
@@ -3,12 +3,14 @@
 
 apt update
 apt install -y software-properties-common rsyslog mailutils php-auth-sasl postfix certbot nano wget
-wget
-wget https://github.com/dlehdgud2380/jupyter_container_management_system/blob/beta/sendmail.sh
+wget https://raw.githubusercontent.com/dlehdgud2380/jupyter_container_management_system/beta/sendmail.sh
+chmod 777 sendmail.sh
 nano /etc/postfix/main.cf
 nano /etc/postfix/sasl/sasl_passwd
 postmap /etc/postfix/sasl/sasl_passwd
 chown root:root /etc/postfix/sasl/sasl_passwd
 chmod 600 /etc/postfix/sasl/sasl_passwd
-sudo service postfix restart
+service rsyslog start
+service postfix restart
 chfn -f "JCMS MailService" root
+echo "Please visit https://myaccount.google.com/lesssecureapps and enable app access"
