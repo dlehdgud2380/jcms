84f1c41c7f7414c2e2a032081c1f468848c1a07e
dlehdgud2380 <dlehdgud2380@gmail.com>
2022-01-18 14:49:03 +0900

    add: mail send function added

--- container.py
+++ container.py
@@ -2,6 +2,7 @@
 
 # 라이브러리 선언을 위한 import
 from dis import dis
+import io
 from os import system, listdir
 import time
 import socket
@@ -17,22 +18,29 @@ from typing import Any, Dict, List
 home_path: str = None  # user 홈 폴더
 work_path: str = None  # 작업 경로
 
-# HOST_IP 주소 확인하는 함수
 
-
-def get_ip_address():
+def get_ip_address() -> None:
+    """
+    HOST_IP 주소 확인하는 함수
+    """
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-# output값이 필요한 shell command 함수
-
 
 def command(command: str) -> str:
-    output: _io.BufferedReader = subprocess.Popen(
+    """
+    output값이 필요한 shell command 함수
+    """
+    output: IO[bytes] = subprocess.Popen(
         f"{command}", shell=True, stdout=subprocess.PIPE).stdout
     result: bytes = output.read()
     return result.decode()
+
+# 이메일 컨테이너 확인하는 함수
+
+
+# def check
 
 
 # user 홈 폴더 경로 가져오기
@@ -50,8 +58,10 @@ else:  # 있으면 패쓰~
 work_path: str = f"{home_path}/jupyter_management_storage"
 
 
-# 컨테이너 관리하는 클래스
 class Container:
+    """
+    컨테이너 관리하는 클래스
+    """
 
     # 자동으로 컨테이너 생성하고 정보 저장
     # 호스트 포트는 8888번 기본값, 지정가능
@@ -66,32 +76,39 @@ class Container:
 
     # 커스텀 도커 이미지 있는 지 체크 하는 함수(Private)
     def __check_image(self) -> None:
-        # 이미지 존재하는 지 확인
-        image_check: str = command("docker images")
+        check: str = command("docker images")  # 이미지 존재하는 지 확인
+
         # 존재하면 pass, 존재하지 않으면 docker image build
-        if "image_watson_jupyter" not in image_check.split():
+        if "image_watson_jupyter" not in check.split():
             print("Not Ready: 커스텀 도커 이미지가 존재하지 않음 -> 이미지 빌드 실행")
             system("docker build -t image_watson_jupyter:0.0.1 . -f jupyter.Dockerfile")
         else:
             print("OK: 커스텀 도커 이미지가 존재합니다.")
 
-    # 컨테이너 생성할때 사용하는 함수
     def init(self) -> None:
+        """
+        컨테이너 생성할때 사용하는 함수
+        """
         self.__check_image()  # 이미지 있는 지 체크
+
         # 포트설정(기본 8888)
         host_port: str = self.port["hostPort"]  # host
         cont_port: str = self.port["conPort"]  # container
+
         # 사용중인 컨테이너 포트 확인 하기
         port_check: str = command("docker ps -a").split()
         while True:
+
             # 겹치는 포트 없으면 스탑 있으면 포트 1씩 더해보기
             if f":::{host_port}->{cont_port}/tcp" not in port_check:
                 break
+
             # 존재하면 호스트 포트 1씩 증가시키기
             else:
                 host_port += 1
         # 하나의 컨테이너의 정보를 담는 디렉터리 생성하기
         system(f"mkdir {work_path}/{self.container_name}")
+
         # 컨테이너 생성 하기
         docker_run_command: str = f"docker run -d -it " \
             f"-p {host_port}:{cont_port} " \
@@ -102,33 +119,47 @@ class Container:
         self.container_id = command(docker_run_command).strip()
         # print(f"id: {self.container_id}")
 
-    # 컨테이너 시작하는 함수
     def start(self) -> None:
+        """
+        컨테이너 시작하는 함수
+        """
+
         # 컨테이너 시작
         system(f"docker start {self.container_name} ")
 
-    # 컨테이너 정지 하는 함수
     def stop(self) -> None:
+        """
+        컨테이너 정지 하는 함수
+        """
+
         # 컨테이너 정지
         system(f"docker stop {self.container_name} ")
 
-    # 컨테이너 삭제 하는 함수
     def remove(self) -> None:
+        """
+        컨테이너 삭제 하는 함수
+        """
+
         # 사용중일수도 있으므로 일단 멈춤
         system(f"docker stop {self.container_name}")
         print("(Success stopped)")
+
         # 하나의 컨테이너의 정보를 담는 디렉터리 제거하기
         system(f"rm -rf {work_path}/{self.container_name}")
         print("(Success Deleted Directory)")
+
         # 컨테이너 제거하기
         system(f"docker rm {self.container_name}")
         print(f"({self.container_name} removed)")
 
-    # 컨테이너 정보 보여주는 함수
-    # Option = True -> tuple형태로 리턴도 하고 print도 함
-    # Option = Flase -> print로 정보만 보여줌 (기본값임)
     def info(self, option: bool = False) -> Dict:
+        """
+        ## 컨테이너 정보 보여주는 함수
+        * Option = True -> tuple형태로 리턴도 하고 print도 함
+        * Option = Flase -> print로 정보만 보여줌 (기본값임)
+        """
         host_port: str = self.port["hostPort"]  # host포트 가져오기
+
         # 컨테이너 정보가 담긴 Dict
         container_info: Dict = {
             "conId": self.container_id,
@@ -137,12 +168,14 @@ class Container:
         }
         print(
             f"[Container Info]\nid: {self.container_id}\nname: {self.container_name}\nport: {host_port}")
-        if option == True:
+        if option is True:
             return container_info
 
 
-# 컨테이너 내의 쥬피터 관련 된 정보 가져오는 클래스
 class JupyterInfo:
+    """
+    컨테이너 내의 쥬피터 관련 된 정보 가져오는 클래스
+    """
 
     def __init__(self, contiainer_info: Dict, ) -> None:
 
@@ -151,7 +184,7 @@ class JupyterInfo:
         self.get_info: Dict = contiainer_info
 
         # 컨테이너 이름 저장
-        self.container_name = contiainer_info['conName']
+        self.container_name: str = contiainer_info['conName']
 
         # 커넥션 정보 dict 형태로 정의
         self.connection_info: Dict = {
@@ -162,8 +195,10 @@ class JupyterInfo:
             "token": None
         }
 
-    # Jupyter 연결에 필요한 정보를 가져와 저장하는 함수
     def info(self) -> None:
+        """
+        Jupyter 연결에 필요한 정보를 가져와 저장하는 함수
+        """
 
         # 정규식 선언
         # regex_port: Any = re.compile(":[0-9]+") # 포트 정보 뽑는 정규식
@@ -190,17 +225,42 @@ class JupyterInfo:
         f.close()
 
 
-# 로그 기록 하는 쓰레드 클래스
+def send_mail(container_name) -> None:
+    """
+    jupyter 접속에 필요한 정보를 이메일로 전송하는 함수
+    """
+
+    mail_container: str = "juMailServer"  # 메일컨테이너 이름
+
+    # 메일에 들어갈 내용 정리
+    # to: str = email
+    # subject: str = f"Hello User! check {container_name} info"  # 제목
+    # 접속정보가 담긴 디렉터리
+    # txt_file: str = f"/jupyter_management_storage/{container_name}/connection_info.txt"
+
+    # 메일 보낼 명령어 입력
+    command: str = f"docker exec -it {mail_container} " \
+        f"su root -c " \
+        f"'echo {container_name} | /bin/bash ./sendmail.sh'"
+    system(command)  # 해당 컨테이너에 대한 메일 전송
+
+
 class Logger(threading.Thread):
+    """
+    로그 기록 하는 쓰레드 클래스
+    """
+
     def __init__(self, container_name: str, display: bool = False) -> None:
         threading.Thread.__init__(self)
         self.container_name = "jupyter_" + container_name
         self.display = display  # 터미널상에 log를 띄울것인지 아닌지
         print(f"[logger thread start - {display}]")
 
-    # 컨테이너 로그 기록 실행하는 함수
-    # 컨테이너 종료되면 같이 종료됨
     def run(self) -> None:
+        """
+        컨테이너 로그 기록 실행하는 함수
+        * 컨테이너 종료되면 같이 종료됨
+        """
         option: str = None  # 기록 옵션(터미널에 띄울것인지 아닌지)
         if self.display is True:
             option = "| tee"
@@ -221,9 +281,11 @@ if __name__ == "__main__":
     jupyter_info.info()
     print("\n\n------------print connection_info.txt------------")
     system(f"cat {work_path}/{con_info['conName']}/connection_info.txt")
-    print("\n\n------------logging------------")
-    logger = Logger("test1")
-    logger.daemon = True
-    logger.start()
+    print("\n\n------------send mail connection info------------")
+    send_mail("jupyter_test1")
+    # print("\n\n------------logging------------")
+    # logger = Logger("test1")
+    # logger.daemon = True
+    # logger.start()
     # print("\n\n------------delete container------------")
     # jupyter_test1.remove()
