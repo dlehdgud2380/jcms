7ac3b9d002283316a3dd40fdfbe241bf6b292e46
dlehdgud2380 <dlehdgud2380@gmail.com>
2022-01-21 17:02:40 +0900

    setting: django.template.context_processors.request applied

--- config/settings.py
+++ config/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -129,3 +130,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = False
